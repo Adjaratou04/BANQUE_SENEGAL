d8b6fc85f225ddd5782d7ab80fc507f89e228ed0
Adjaratou04 <159186759+Adjaratou04@users.noreply.github.com>
2026-03-16 21:17:49 +0000

    Update mongo_connection.py

--- database/mongo_connection.py
+++ database/mongo_connection.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 def get_database():
     uri = os.getenv("MONGO_URI", "mongodb://localhost:27017")
-    db_name = os.getenv("MONGO_DB_NAME", "Banque_senegal")
+    db_name = os.getenv("MONGO_DB_NAME", "Baqnues_senegal")
     client = MongoClient(uri)
     db = client[db_name]
     return db
